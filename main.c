00d9861daaf08f825452febe10da347639b8e96a
İbrahim Hakkı Ergin <ibrahimh.ergin@gmail.com>
2026-04-16 13:10:01 +0300

    add: bench skeleton (not finished)

--- main.c
+++ main.c
@@ -6,46 +6,40 @@
 /*   By: iergin <iergin@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/01 15:28:15 by iergin            #+#    #+#             */
-/*   Updated: 2026/04/13 15:47:28 by iergin           ###   ########.fr       */
+/*   Updated: 2026/04/16 13:09:26 by iergin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h>
 
-static void	select_mode(char *arg, int *mode, int *i)
+static void	select_mode(char **args, int *mode, int *i)
 {
-	if (ft_strncmp(arg, "--simple", 9) == 0)
+	while (args[*i] && ft_strncmp(args[*i], "--", 2) == 0)
 	{
-		*mode = 1;
-		*i = 2;
-	}
-	else if (ft_strncmp(arg, "--medium", 9) == 0)
-	{
-		*mode = 2;
-		*i = 2;
-	}
-	else if (ft_strncmp(arg, "--complex", 10) == 0)
-	{
-		*mode = 3;
-		*i = 2;
-	}
-	else if (ft_strncmp(arg, "--adaptive", 11) == 0)
-	{
-		*mode = 0;
-		*i = 2;
+		if (ft_strncmp(args[*i], "--simple", 9) == 0)
+			*mode = (*mode / 10) * 10 + 1;
+		else if (ft_strncmp(args[*i], "--medium", 9) == 0)
+			*mode = (*mode / 10) * 10 + 2;
+		else if (ft_strncmp(args[*i], "--complex", 10) == 0)
+			*mode = (*mode / 10) * 10 + 3;
+		else if (ft_strncmp(args[*i], "--adaptive", 11) == 0)
+			*mode = (*mode / 10) * 10 + 0;
+		else if (ft_strncmp(args[*i], "--bench", 8) == 0)
+			*mode += 10;
+		(*i)++;
 	}
 }
 
 static void	select_sort(t_stack **stack_a, int *mode, double disorder)
 {
-	if (*mode == 1)
+	if (*mode % 10 == 1)
 		selection_sort(stack_a);
-	else if (*mode == 2)
+	else if (*mode % 10 == 2)
 		k_sort(stack_a);
-	else if (*mode == 3)
+	else if (*mode % 10 == 3)
 		radix_sort(stack_a);
-	else if (*mode == 0)
+	else if (*mode % 10 == 0)
 	{
 		if (disorder != 0 && disorder < 0.2)
 			selection_sort(stack_a);
@@ -75,6 +69,37 @@ static int	fill_stack(t_stack **stack_a, int argc, char **args, int i)
 	return (1);
 }
 
+static void	print_strategy(int mode, double disorder)
+{
+	if (mode % 10 == 1)
+		printf("[bench] strategy: Simple / O(n^2)\n");
+	else if (mode % 10 == 2)
+		printf("[bench] strategy: Medium / O(n√n)\n");
+	else if (mode % 10 == 3)
+		printf("[bench] strategy: Complex / O(n log n)\n");
+	else if (mode % 10 == 0)
+	{
+		if (disorder < 0.2)
+			printf("[bench] strategy: Adaptive / O(n^2)\n");
+		else if (disorder >= 0.2 && disorder < 0.5)
+			printf("[bench] strategy: Adaptive / O(n√n)\n");
+		else
+			printf("[bench] strategy: Adaptive / O(n log n)\n");
+	}
+}
+
+static void	benchmark(int mode, double disorder)
+{
+	int	total_ops;
+
+	total_ops = 0;
+	printf("[bench] disorder: %.2f%%\n", disorder * 100);
+	print_strategy(mode, disorder);
+	printf("[bench] total_ops: %d\n", total_ops);
+	printf("[bench] sa: 0 sb: 0 ss: 0 pa: 0 pb: 0\n");
+	printf("[bench] ra: 0 rb: 0 rr: 0 rra: 0 rrb: 0 rrr: 0\n");
+}
+
 int	main(int argc, char **args)
 {
 	t_stack	*stack_a;
@@ -88,12 +113,15 @@ int	main(int argc, char **args)
 	i = 1;
 	mode = 0;
 	stack_a = NULL;
-	if (argc > 1)
-		select_mode(args[1], &mode, &i);
+	select_mode(args, &mode, &i);
 	err = fill_stack(&stack_a, argc, args, i);
 	disorder = compute_disorder(&stack_a);
 	if (err)
+	{
 		select_sort(&stack_a, &mode, disorder);
+		if (mode >= 10)
+			benchmark(mode, disorder);
+	}
 	ft_lstclear(&stack_a);
 	return (0);
 }
