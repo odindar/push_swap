f322c2a1c3f44a707469e8a490b03f2deb875a11
İbrahim Hakkı Ergin <ibrahimh.ergin@gmail.com>
2026-04-24 23:10:23 +0300

    update: norminette and stack len optimization

--- radix.c
+++ radix.c
@@ -6,7 +6,7 @@
 /*   By: iergin <iergin@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/08 21:55:29 by iergin            #+#    #+#             */
-/*   Updated: 2026/04/24 22:29:55 by iergin           ###   ########.fr       */
+/*   Updated: 2026/04/24 23:09:10 by iergin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,26 @@ static int	max_idx_bit(int x)
 	return (res);
 }
 
+static void	radix(t_stack **stack_a, t_stack **stack_b, t_bench *b, int i)
+{
+	int		cur_stack_a_len;
+
+	cur_stack_a_len = stack_len(stack_a);
+	while (cur_stack_a_len > 0)
+	{
+		if (((*stack_a)->index >> i) & 1)
+			ra(stack_a, b);
+		else
+			pb(stack_a, stack_b, b);
+		cur_stack_a_len--;
+	}
+}
+
 void	radix_sort(t_stack **stack_a, t_bench *b)
 {
 	t_stack	*stack_b;
 	int		i;
-	int		cur_stack_a_len;
+	int		max_bit;
 
 	i = 0;
 	stack_b = NULL;
@@ -38,17 +53,10 @@ void	radix_sort(t_stack **stack_a, t_bench *b)
 	if (handle_small_sort(stack_len(stack_a), stack_a, b))
 		return ;
 	assign_index(stack_a);
-	while (i < max_idx_bit(stack_len(stack_a) - 1))
+	max_bit = max_idx_bit(stack_len(stack_a) - 1);
+	while (i < max_bit)
 	{
-		cur_stack_a_len = stack_len(stack_a);
-		while (cur_stack_a_len > 0)
-		{
-			if (((*stack_a)->index >> i) & 1)
-				ra(stack_a, b);
-			else
-				pb(stack_a, &stack_b, b);
-			cur_stack_a_len--;
-		}
+		radix(stack_a, &stack_b, b, i);
 		push_b_to_a(stack_a, &stack_b, b);
 		i++;
 	}
